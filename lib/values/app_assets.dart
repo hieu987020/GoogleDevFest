f0ad861abdb743df19e36c803e962d7cba3abf4e
nguyennguyenloc <nnloc123@gmail.com>
2021-12-07 20:16:12 +0700

    upload_layout

---
+++ lib/values/app_assets.dart
@@ -0,0 +1,5 @@
+class AppAssets {
+  static String imagePath = 'images/';
+  static String imageSyringe = imagePath + 'syringe.png';
+  // log(imageSyringe);
+}
---
+++ lib/values/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xff0B0F2F);
+  static const Color mainColor1 = Color(0xff4768FE);
+  static const Color mainColor2 = Color(0xff3D54F8);
+  static const Color getstartColor1 = Color(0xffFFFFFF);
+  static const Color getstartColor2 = Color(0xff18CCFF);
+  static const Color loginColor = Color(0xff15284D);
+  static const Color boderFocus = Color(0xff3D58F8);
+
+  static const Color textMainColor = Color(0xffFFFFFF);
+  static const Color textMainTwoColor = Color(0xffE0DADA);
+}
